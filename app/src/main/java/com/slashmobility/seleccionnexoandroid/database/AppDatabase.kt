53672e7803dbdd5e224ac728be7a387baba22227
Leonardo Lovera <leolovera09@gmail.com>
2019-12-02 14:56:49 -0300

    Group detail ViewModel

--- app/src/main/java/com/slashmobility/seleccionnexoandroid/database/AppDatabase.kt
+++ app/src/main/java/com/slashmobility/seleccionnexoandroid/database/AppDatabase.kt
@@ -2,15 +2,19 @@ package com.slashmobility.seleccionnexoandroid.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.slashmobility.seleccionnexoandroid.database.daos.GroupDao
 import com.slashmobility.seleccionnexoandroid.database.daos.GroupImagesDao
 import com.slashmobility.seleccionnexoandroid.models.Group
+import com.slashmobility.seleccionnexoandroid.models.GroupImages
+import com.slashmobility.seleccionnexoandroid.models.ImagesConverter
 
 /**
  * Created by Leo ¯\_(ツ)_/¯ on 2019-12-02
  */
 
-@Database(entities = [Group::class], version = 2, exportSchema = false)
+@Database(entities = [Group::class, GroupImages::class], version = 2, exportSchema = false)
+@TypeConverters(ImagesConverter::class)
 abstract class AppDatabase : RoomDatabase() {
 
     abstract fun groupDao(): GroupDao
--- app/src/main/java/com/slashmobility/seleccionnexoandroid/models/GroupImages.kt
+++ app/src/main/java/com/slashmobility/seleccionnexoandroid/models/GroupImages.kt
@@ -3,7 +3,10 @@ package com.slashmobility.seleccionnexoandroid.models
 import android.os.Parcelable
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import androidx.room.TypeConverter
+import com.google.gson.Gson
 import com.google.gson.annotations.SerializedName
+import com.google.gson.reflect.TypeToken
 import kotlinx.android.parcel.Parcelize
 
 /**
@@ -20,5 +23,20 @@ data class GroupImages(
     fun customMethod(): String {
 
         return ""
+    }
+}
+
+class ImagesConverter {
+
+    private val gson = Gson()
+
+    @TypeConverter
+    fun arrayListToJson(list: ArrayList<String>?): String? {
+        return if(list == null) null else gson.toJson(list)
+    }
+
+    @TypeConverter
+    fun jsonToArrayList(jsonData: String?): ArrayList<String>? {
+        return if (jsonData == null) null else gson.fromJson(jsonData, object : TypeToken<ArrayList<String>?>() {}.type)
     }
 }
